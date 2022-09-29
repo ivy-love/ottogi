55e0902f6ab89406421c426e38176cadb49d3c5a
Jihee <62944687+ivy-love@users.noreply.github.com>
2022-09-29 17:12:29 +0900

    Add commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,40 +3,36 @@ $(document).ready(function () {
     //aos animate
     AOS.init();
 
-    
     //coupon close 클릭 후 slideup
     $(".coupon_close").click(function () {
         $(".coupon").slideUp(150);
     });
 
-
     //coupon close 클릭 후 body 올리기
     $(".coupon_close").click(function () {
-        if($(".header").scrollTop(0)) {
+        if ($(".header").scrollTop(0)) {
             $("body").animate({
                 "margin-top": "-45px"
             }, 160);
-        }
-    })
+        };
+    });
 
-
-    //gnb animate
+    //gnb
     $(".gnb_list_warpper").hover(function () {
         $(".gnb_bg").stop().animate({
             height: 300
-        }, 200)
+        }, 200);
         $(this).find(".gnb_list_container").stop().animate({
             height: 300
-        }, 300)
+        }, 300);
     }, function () {
         $(".gnb_list_container").stop().animate({
             height: 0
-        }, 50)
+        }, 50);
         $(".gnb_bg").stop().animate({
             height: 0
-        }, 300)
-    })
-
+        }, 300);
+    });
 
     //main_swiper
     var swiper = new Swiper(".main_banner .main_swiper", {
@@ -54,7 +50,6 @@ $(document).ready(function () {
         },
     });
 
-
     //recipe_swiper
     var swiper = new Swiper(".recipe_item .recipe_swiper", {
         loop: true,
@@ -68,42 +63,28 @@ $(document).ready(function () {
         },
     });
 
-
     //best_keyword 탭메뉴
-    $(".keyword_list").click(function(){
-        const idx = $(this).index();
+    $(".keyword_list").click(function () {
+        idx = $(this).index() + 1;
 
         $(".menu_item").removeClass("active");
-        $(".menu_item").eq(idx).addClass("active");
+        $("#a" + idx).addClass("active");
 
         $(".keyword_list").removeClass("active");
-        $(".keyword_list").eq(idx).addClass("active");
-    })
-
+        $(this).addClass("active");
+    });
 
     //familysite_btn
     $(".familysite_btn").click(function () {
-
-        const cnt = $(".content_list").length;
-
-        if($(".familysite_content_wrapper").height() == 0) {
+        cnt = $(".familysite_content_wrapper .content_list").length;
+        if ($(".familysite_content_wrapper").height() == 0) {
             $(".familysite_content_wrapper").stop().animate({
                 height: cnt * 52
-            },100)
-        }else {
+            }, 100)
+        } else {
             $(".familysite_content_wrapper").stop().animate({
                 height: 0
-            },100)
-        }
-    })
-
-
-    // familysite_btn 숨기기
-    $(document).on('click', function() {
-        if(!$(event.target).hasClass("familysite_btn")) {
-        $(".familysite_content_wrapper").stop().animate({
-            height: 0
-        },100)
-        }
-    })
-})
+            }, 100)
+        };
+    });
+});
